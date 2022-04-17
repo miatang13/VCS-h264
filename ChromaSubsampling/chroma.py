c93847d10b8ab6bc65423c05ce5e2d71095f8ead
miatang13 <miatang13@gmail.com>
2022-04-17 16:53:44 -0400

    Add convert compressed Ycrcb back to BGR

--- ChromaSubsampling/chroma.py
+++ ChromaSubsampling/chroma.py
@@ -1,8 +1,10 @@
-# import the cv2 library
 import cv2
+import numpy as np
 
-# The function cv2.imread() is used to read an image.
 img = cv2.imread('test.jpg')
+rows,cols,channels = img.shape
+
+# TODO: We do BGR to YCRCB manually
 imgYYC = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
 
 # We do 2x2 subsampling
@@ -15,9 +17,27 @@ cb = cv2.boxFilter(imgYYC[:,:,2],ddepth=-1,ksize=ksize)
 
 # Subsample
 crSamples = cr[::sampRate, ::sampRate]
-cbSamples = cr[::sampRate, ::sampRate]
+cbSamples = cb[::sampRate, ::sampRate]
 
 # Final subsampled
 subsampledImg = [imgYYC[:,:,0], crSamples, cbSamples]
+print(len(subsampledImg[0]), len(subsampledImg[1]))
 
+# Convert YCrCb back to BGR for display
+finalImg = np.zeros((rows, cols, channels), dtype = np.uint8)
+for i in range(rows):
+    for j in range(cols):
+        Y = subsampledImg[0][i, j]
+        Cr = crSamples[int(i/2), int(j/2)]
+        Cb = cbSamples[int(i/2), int(j/2)]
+        r = Y + 1.4022 * (Cr - 128)
+        g = Y - 0.34414 * (Cb - 128) - 0.71414 * (Cr - 128)
+        b = Y + 1.77200 * (Cb - 128)
+        r = max(0, min(255, r))
+        g = max(0, min(255, g))
+        b = max(0, min(255, b))
+        finalImg[i, j] = [b, g, r]
 
+# Output image
+cv2.imwrite("Output.jpg", finalImg)
+print("Finished writing image")
