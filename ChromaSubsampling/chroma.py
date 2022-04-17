7862df671a14bc8e11c11a941e4b64a3c75438e9
miatang13 <miatang13@gmail.com>
2022-04-17 17:08:41 -0400

    Add comparison plot

--- ChromaSubsampling/chroma.py
+++ ChromaSubsampling/chroma.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-img = cv2.imread('test.jpg')
+img = cv2.imread('../images/happy-corgi.jpg')
 rows,cols,channels = img.shape
 
 # TODO: We do BGR to YCRCB manually
 imgYYC = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
 
-# We do 2x2 subsampling
+# We do 4:2:0 subsampling
 sampRate = 2
 ksize = (2, 2)
 
@@ -28,8 +29,8 @@ finalImg = np.zeros((rows, cols, channels), dtype = np.uint8)
 for i in range(rows):
     for j in range(cols):
         Y = subsampledImg[0][i, j]
-        Cr = crSamples[int(i/2), int(j/2)]
-        Cb = cbSamples[int(i/2), int(j/2)]
+        Cr = crSamples[int(i/sampRate), int(j/sampRate)]
+        Cb = cbSamples[int(i/sampRate), int(j/sampRate)]
         r = Y + 1.4022 * (Cr - 128)
         g = Y - 0.34414 * (Cb - 128) - 0.71414 * (Cr - 128)
         b = Y + 1.77200 * (Cb - 128)
@@ -37,6 +38,17 @@ for i in range(rows):
         g = max(0, min(255, g))
         b = max(0, min(255, b))
         finalImg[i, j] = [b, g, r]
+
+# show image with matplotlib
+plt.figure(figsize=(1, 2)) # specifying the overall grid size
+images = [cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cv2.cvtColor(finalImg, cv2.COLOR_BGR2RGB)]
+labels = ["Input Image", "Output Image"]
+for i in range(2):
+    plt.subplot(1,2,i+1)    # the number of images in the grid is 1*2 (2)
+    plt.imshow(images[i])
+    plt.xlabel(labels[i])
+
+plt.show()
 
 # Output image
 cv2.imwrite("Output.jpg", finalImg)
