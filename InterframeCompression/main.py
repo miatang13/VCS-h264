a42eb85a3d260e1a9a86028ba95017dc7501fa26
miatang13 <miatang13@gmail.com>
2022-04-20 12:36:35 -0400

    Add optical flow reference code
    - Output video code is still not working

--- InterframeCompression/main.py
+++ InterframeCompression/main.py
@@ -3,14 +3,28 @@ import cv2
 
 cap = cv2.VideoCapture('../videos/corgi_underwater_cut.mp4')
 
-while(cap.isOpened()):
+# Define the codec and create VideoWriter object
+# https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html
+fps = 20.0
+fourcc = cv2.VideoWriter_fourcc(*'X264')
+out = cv2.VideoWriter('output.mkv',fourcc, fps, (640,480))
+
+while cap.isOpened():
     ret, frame = cap.read()
-
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    print("Flipping frame")
+    frame = cv2.flip(frame, 0)
+    # write the flipped frame
+    out.write(frame)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
         break
 
+# Release everything if job is finished
+print("Releasing everything. Job finished. ")
 cap.release()
+out.release()
+
 cv2.destroyAllWindows()
