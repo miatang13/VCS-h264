ea7a40c159bf1edbfb3d6683de32d7ba06ce86a3
miatang13 <miatang13@gmail.com>
2022-04-30 17:09:16 -0400

    Add test images, set up writing to file

--- InterframeCompression/main.py
+++ InterframeCompression/main.py
@@ -2,21 +2,24 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+import json
 
 TEST_VIDEO = False
 TEST_IMG = not TEST_VIDEO
+SAVE_FRAMES = False
 
 fig, ax = plt.subplots()
 
 frames = []
+macro_blocks = []
 
 ######################################################################
 
 
 class Frame:
-    def __init__(self, frame_type, macro_blocks):
+    def __init__(self, frame_type, macro_blocks_idx):
         self.frame_type = frame_type
-        self.macro_blocks = macro_blocks
+        self.macro_blocks_idx = macro_blocks_idx
 
 
 class Macroblock:
@@ -50,9 +53,32 @@ def split_frame_into_mblocks(input_frame):
             ax.add_patch(Rectangle((start_i, start_j), block_size,
                          block_size,  edgecolor='black', fill=False, lw=0.5))
     print("Finished splitting frame into macro blocks")
+    return blocks
 
 # For each block in each (P, B) frame, we try to find a similar block in an I frame
 # Then we can save space by encoding that Macroblock with just a motion vector
+
+
+def match_block():
+    img = imread('https://i.stack.imgur.com/JL2LW.png', pilmode='L')
+    temp = imread('https://i.stack.imgur.com/UIUzJ.png', pilmode='L')
+
+    corr = sp.correlate2d(img - img.mean(),
+                          temp - temp.mean(),
+                          boundary='symm',
+                          mode='full')
+
+
+def process_B_frame():
+    return
+
+
+def process_P_frame():
+    return
+
+
+def process_I_frame():
+    return
 
 # Display order of frames: I, B, P, B, P, B, P
 
@@ -61,7 +87,8 @@ order_length = 7
 
 
 def encode_frame(input_frame, frame_num):
-    macro_blocks = split_frame_into_mblocks(input_frame)
+    blocks = split_frame_into_mblocks(input_frame)
+    macro_blocks.append(blocks)
     frame_num %= order_length
     frame_type = ""
     if (frame_num == 0):
@@ -71,19 +98,42 @@ def encode_frame(input_frame, frame_num):
     else:
         frame_type = "P"
     print("Encode frame of type", frame_type)
-    new_frame = Frame(frame_type, macro_blocks)
+    new_frame = Frame(frame_type, len(macro_blocks)-1)
     frames.append(new_frame)
     return
 
 
 ######################################################################
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
 if TEST_IMG:
-    img = cv2.imread('../images/happy-corgi(8x8).jpg')
-    vwidth, vheight, channels = img.shape
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    plt.imshow(img)
-    encode_frame(img, 0)
-    plt.show()
+    img1 = cv2.imread('../images/corgi-underwater/12.jpg')
+    img2 = cv2.imread('../images/corgi-underwater/20.jpg')
+    vwidth, vheight, channels = img1.shape
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+    plt.imshow(img1)
+    encode_frame(img1, 0)
+    encode_frame(img2, 1)
+
+    # Save compressed data of frames
+    json_string = json.dumps([ob.__dict__ for ob in frames])
+    with open("frames.json", "w") as text_file:
+        print(f"{json_string}", file=text_file)
+
+    if SAVE_FRAMES:
+        # Save frames
+        # TODO: replace with compressed inter frame data
+        # https://stackoverflow.com/questions/26646362/numpy-array-is-not-json-serializable
+        json_dump = json.dumps({'a': macro_blocks, 'aa': [2, (2, 3, 4), macro_blocks], 'bb': [2]},
+                               cls=NumpyEncoder)
+        with open("blocks.json", "w")as text_file:
+            print(f"{json_dump}", file=text_file)
+    # plt.show()
 
 
 ######################################################################
